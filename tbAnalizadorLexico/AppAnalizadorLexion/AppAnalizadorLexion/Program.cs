32de3cb06a0c381c86cc61899883f0cef564aa66
dbsalazarr <dbsalazarr@gmail.com>
2024-10-28 02:10:03 -0500

    Analizador Lexico - tb terminado

--- tbAnalizadorLexico/AppAnalizadorLexion/AppAnalizadorLexion/Program.cs
+++ tbAnalizadorLexico/AppAnalizadorLexion/AppAnalizadorLexion/Program.cs
@@ -1,20 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using AnalizadorLexico;
-
 
 
 class Program
 {
     static void Main(string[] args)
     {
-        string codigoFuente;
+        string codigoPrograma;
 
-        codigoFuente = Console.ReadLine();
-        CScanner scanner = new CScanner(codigoFuente);
+        // codigoFuente = Console.ReadLine();
+        codigoPrograma = "# Función saludo, retorna un string, tiene un parámetro asignado\r\ndef saludar(nombre):\r\n    return \"Bienvenido\" + nombre\r\n\r\n\r\n";
 
-        Console.WriteLine("Hola Bremdow");
+        CScanner scanner = new CScanner(codigoPrograma);
+
+        // Recuperando los tokens a partir de la función Analizar la cual hace uso de la FSM
+        List<(CScanner.TokenType, string)> tokens = scanner.Analizar();
+
+        Console.WriteLine(codigoPrograma);
+        foreach ( var token in tokens)
+        {
+            Console.WriteLine($"{token.Item1} : {token.Item2}");
+        }
+
         Console.ReadKey();
+
     }
 }
-
-
