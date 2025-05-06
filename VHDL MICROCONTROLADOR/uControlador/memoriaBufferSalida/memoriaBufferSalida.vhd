e6e0f83befe4b2c7d2e6fe54e45dd014a2c6478b
dbsrunsaac <dbsrunsaac@unsaac.edu.pe>
2025-05-06 13:41:30 -0500

    RPU Pipeline

--- VHDL MICROCONTROLADOR/uControlador/memoriaBufferSalida/memoriaBufferSalida.vhd
+++ VHDL MICROCONTROLADOR/uControlador/memoriaBufferSalida/memoriaBufferSalida.vhd
@@ -18,7 +18,7 @@ architecture Arch of memoriaBufferSalida is
 	type Memory_Array is array (0 to 255) of STD_LOGIC_VECTOR(7 downto 0);
 	--signal Memory : Memory_Array := (others => (others => '0')); -- Memoria inicializada en 0
 	signal Memory : Memory_Array := (
-		0 => "00010001", 1 => "00000001", 2 => "00000000", 3 => "00000000", 4 => "00000000", 5 => "00000000", 6 => "00000000", 7 => "00000000",
+		0 => "00010001", 1 => "00000001", 2 => "00000000", 3 => "00000100", 4 => "00000000", 5 => "00000000", 6 => "00000000", 7 => "00000000",
 		8 => "00000000", 9 => "00000000", 10 => "00000000", 11 => "00000000", 12 => "00000000", 13 => "00000000", 14 => "00000000", 15 => "00000000",
 		16 => "00000000",
 		others => "00000000" -- Resto de valores en cero 
